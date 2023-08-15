264952d877aadfcdf7f105a5e624ca1d539b3e78
Miguel Nunes <miguelnunes@criticalmanufacturing.com>
2023-08-15 17:29:27 +0100

    disable logs in production

--- folha-de-obra-app/src/main.tsx
+++ folha-de-obra-app/src/main.tsx
@@ -6,6 +6,14 @@ import "./index.css";
 import store from "./store/store";
 import { Provider } from "react-redux";
 
+// replace console.* for disable log on production
+console.log(import.meta.env.MODE);
+if (import.meta.env.MODE === 'production') {
+  console.log = () => {}
+  console.error = () => {}
+  console.debug = () => {}
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
