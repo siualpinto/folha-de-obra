3fbfb31bd862cc760848ac88d5c4a52c4cc885a9
Miguel Nunes <miguelnunes@criticalmanufacturing.com>
2023-08-15 17:10:54 +0100

    update store on client form changes

--- folha-de-obra-app/src/components/common/Inputs/Input.tsx
+++ folha-de-obra-app/src/components/common/Inputs/Input.tsx
@@ -2,15 +2,23 @@ interface Props {
   id: string;
   label: string;
   inputType?: "text" | "number";
+  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined,
+  value: string | ReadonlyArray<string> | number | undefined
 }
 
-const Input = ({ id, label, inputType = "text" }: Props) => {
+const Input = ({ id, label, inputType = "text", onChange, value }: Props) => {
   return (
     <div>
       <label htmlFor={id} className="form-label">
         {label}
       </label>
-      <input id={id} type={inputType} className="form-control" />
+      <input
+        id={id}
+        type={inputType}
+        className="form-control"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- folha-de-obra-app/src/components/form/client/Client.tsx
+++ folha-de-obra-app/src/components/form/client/Client.tsx
@@ -1,14 +1,44 @@
 import Input from "../../common/Inputs/Input";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "../../../store/features/form/formSlice";
+import { Order } from "../../../store/model/order";
 
 const Client = () => {
+  const dispatch = useDispatch();
+  const formState = useSelector((state: any) => state.formReducer)
+    .value as Order;
+
   return (
     <>
       <div className="col-md-3">
         <h3>Cliente</h3>
-        <Input id="ClientName" label="Nome" />
-        <Input id="PersonName" label="Pessoa de contacto" />
-        <Input id="Email" label="Email" />
-        <Input id="Phone" inputType="number" label="Telemovel" />
+        <Input
+          id="ClientName"
+          label="Nome"
+          value={formState.Client.Name}
+          onChange={(e) => dispatch(actions.setClientName(e.target.value))}
+        />
+        <Input
+          id="PersonName"
+          label="Pessoa de contacto"
+          value={formState.Client.ContactPerson}
+          onChange={(e) =>
+            dispatch(actions.setClientContactPerson(e.target.value))
+          }
+        />
+        <Input
+          id="Email"
+          label="Email"
+          value={formState.Client.Email}
+          onChange={(e) => dispatch(actions.setClientEmail(e.target.value))}
+        />
+        <Input
+          id="Phone"
+          inputType="number"
+          label="Telemovel"
+          value={formState.Client.Phone}
+          onChange={(e) => dispatch(actions.setClientPhone(e.target.value))}
+        />
       </div>
     </>
   );
